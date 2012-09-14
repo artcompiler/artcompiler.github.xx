59edf83f314f59dd4a09544b015233f265db1edc
Jeff Dyer <jeffdyer@acm.org>
2012-09-13 17:00:37 -0700

    update text

--- demos/codemap/codemap.js
+++ demos/codemap/codemap.js
@@ -7,63 +7,75 @@
     var rootNode
 
     d3.json(mapName+".js.l7.l9", function(ast) {
-	makeNodeMap(ast, "root")
-	rootNode.hidden = false
-	startTree()
-	startText()
-	startGraph()
-	this.rootNode = rootNode
+	d3.json(mapName+".js.l7.l11", function(edges) {
+	    makeNodeMap(ast, "root")
+	    makeEdgeMap(edges)
+	    rootNode.hidden = false
+	    startTree()
+	    startText()
+	    startGraph()
+	    this.rootNode = rootNode
+	})
     })
 
     // functions
     function makeNodeMap(ast, parent) {
 	if ($.isArray(ast)) {
 	    $.each(ast, function(i, v) {
-		if ($.isArray(v)) {
-		    makeNodeMap(v, parent)
-		}
-		if (v.id) {
-		    nodeMap[v.id] = v
-		    nodeIdList.push(v.id)
-		    v.hidden = true
-		    v.parent = parent
-		}
-		if (v.class == "Fixture") {
-		    fixtureMap[v.id] = v
-		} 
-		if (v.class == "Program") {
-		    rootNode = v
-		}
-
-		if (v.tag === "path") {
-		    var nn = v.id.split("N")
-		    var n0 = "N" + nn[1]
-		    var n1 = "N" + nn[2]
-		    if (edgeMap[n0] === void 0) {
-			edgeMap[n0] = [ ]
-		    }
-		    if (edgeMap[n1] === void 0) {
-		    edgeMap[n1] = [ ]
-		    }
-		    edgeMap[n0].push(n1)
-		    edgeMap[n1].push(n0)
-		}
-		
-		if (v.elts) {
-		    makeNodeMap(v.elts, v.id)
-		}
-	    })
-		}
-	if (ast.id) {
-	    nodeMap[ast.id] = ast
-	    nodeIdList.push(ast.id)
-	    ast.hidden = true
-	    ast.parent = parent
+		makeNodeMap(v, parent)
+	    });
 	}
-	if (ast.elts) {
-	    makeNodeMap(ast.elts, ast.id)
+	else {
+	    if (ast.id) {
+		nodeMap[ast.id] = ast
+		nodeIdList.push(ast.id)
+		ast.hidden = true
+		ast.parent = parent
+	    }
+	    if (ast.class == "Fixture") {
+		fixtureMap[ast.id] = ast
+	    } 
+	    if (ast.class == "Program") {
+		rootNode = ast
+	    }
+	    if (ast.elts) {
+		makeNodeMap(ast.elts, ast.id)
+	    }
 	}
 
+    }
+
+    function makeEdgeMap(edges) {
+	$.each(edges, function (i, v) {
+	    var n0 = nodeMap[v.id]
+	    var n0Parents = getParents(n0.id)    // parents include original child
+	    $.each(v.edges, function (i, v) {
+		var n1 = nodeMap[v.id]
+		var n1Parents = getParents(n1.id)
+		$.each(n0Parents, function (i0, v0) {
+		    $.each(n1Parents, function (i1, v1) {
+			if (edgeMap[v0] === void 0) {
+			    edgeMap[v0] = []
+			}
+			if (edgeMap[v1] === void 0) {
+			    edgeMap[v1] = []
+			}
+			edgeMap[v0].push(v1)
+			edgeMap[v1].push(v0)
+		    });
+		});
+	    });
+	});
+    }
+
+    function getParents(n) {
+	var parents = []
+	var node = nodeMap[n]
+	while (node.parent) {
+	    parents.push(node.parent)
+	    node = node.parent
+	}
+	return parents
     }
 
 
