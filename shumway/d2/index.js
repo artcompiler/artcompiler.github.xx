6094ac14785eb4047c653a9176320f5887164f44
Jeff Dyer <jeffdyer@acm.org>
2013-05-14 14:59:08 -0700

    refresh from p105

--- shumway/d2/index.js
+++ shumway/d2/index.js
@@ -1,34 +1,83 @@
 /* -*- Mode: js; js-indent-level: 2; indent-tabs-mode: nil; tab-width: 2 -*- */
+
+var redraw;
+
 (function () {
   var diameter = 1200;
 
-  var tree = d3.layout.tree()
-    .size([360, diameter / 2 - 120])
-    .separation(function(a, b) { return (a.parent == b.parent ? 1 : 2) / a.depth; });
 
-  var diagonal = d3.svg.diagonal.radial()
-    .projection(function(d) { return [d.y, d.x / 180 * Math.PI]; });
+var sites = [
+  "www.youtube.com",
+  "beta.abc.go.com_shows",
+  "live.wsj.com",
+  "movies.uk.msn.com",
+  "video.foxnews.com",
+  "vimeo.com",
+  "www.aljazeera.com_video_",
+  "www.bbc.co.uk",
+  "www.bing.com_?scope=video",
+  "www.cbc.ca_player",
+  "www.cnn.com_video",
+  "www.dailymotion.com",
+  "www.funnyordie.com",
+  "www.grindtv.com",
+  "www.guardian.co.uk_video",
+  "www.hulu.com",
+  "www.liveleak.com",
+  "www.nbc.com_video_",
+  "www.ted.com",
+  "www.twitch.tv",
+  "www.ustream.tv_new",
+  "www.vevo.com",
+  "www.facebook.com",
+];
 
-  var svg = d3.select("body").append("svg")
+  var tr = d3.select(".chooser").selectAll("span").data(sites).enter().append("span")
+ 
+  tr.append("input")
+      .attr("type", "radio")
+      .attr("name", "site")
+      .attr("value", function (d) {
+        return d;
+      })
+      .attr("title", function (d) {
+        return d;
+      })
+      .attr("onclick", function (d) { return "redraw('"+this.value+"')"})
+
+  tr.append("text")
+      .text(function (d) { return d; });
+
+  tr.select("input[value='www.youtube.com']").attr("checked", "true");
+
+  var rootNode;
+  var color = 0x0fffff
+  var nodes, links;
+  d3.json("graph.json", function(r) {
+    rootNode = r;  // set global
+    redraw("www.youtube.com");
+  });
+
+  redraw = function redraw(site) {
+    d3.select("body").selectAll("svg").remove();
+
+    svg = d3.select("body").append("svg")
     .attr("width", diameter)
     .attr("height", diameter)
     .append("g")
     .attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
+    var r = new TreeTrimmer({site: site}).trim(JSON.parse(JSON.stringify(rootNode)));
 
-  var root;
-  var color = 0x0fffff
-  var nodes, links;
-  d3.json("graph.json", function(r) {
-    root = r;  // set global
-    redraw(r);
-  });
+    var tree = d3.layout.tree()
+      .size([360, diameter / 2 - 120])
+      .separation(function(a, b) { return (a.parent == b.parent ? 1 : 2) / a.depth; });
 
-  function redraw(root) {
-    root = new TreeTrimmer({site: "www.youtube.com"}).trim(root);
-//    root = new TreeTrimmer({site: "www.facebook.com"}).trim(root);
-    nodes = tree.nodes(root);
+    nodes = tree.nodes(r);
     links = tree.links(nodes);
 
+    var diagonal = d3.svg.diagonal.radial()
+      .projection(function(d) { return [d.y, d.x / 180 * Math.PI]; });
+    
     var link = svg.selectAll(".link")
       .data(links)
 
@@ -47,25 +96,31 @@
       .attr("class", "node")
       .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ") translate(" + d.y + ")"; })
 
+    var red = "rgb(246, 21, 49)";
+    var yellow = "rgb(255, 199, 11)";
+    var blue = "rgb(0, 92, 173)";
+
     enter.append("circle")
       .attr("r", 3.5)
-      .attr("onclick", "filter(evt.target)")
       .style("fill", function (d) {
         var rgb;
         if (d.status == 1) {
-          return "green";
+          return blue;
         } else if (d.status < .1) {
-          return "red";
+          return red;
         }
-        return "yellow";
+        return yellow;
       })
-//      .style("stroke", "green");
-
-    enter.append("text")
-      .attr("dy", ".31em")
-      .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
-      .attr("transform", function(d) { return d.x < 180 ? "translate(8)" : "rotate(180) translate(-8)"; })
-      .text(function(d) {
+      .style("stroke", function (d) {
+        var rgb;
+        if (d.status == 1) {
+          return blue;
+        } else if (d.status < .1) {
+          return red;
+        }
+        return yellow;
+      })
+      .attr("title", function(d) {
         var status = d.status;
         if (status < 1 && status > 0 && status != .5) {
           status = " " + Math.round(d.status * 100) + "%";
@@ -74,6 +129,25 @@
         }
         return d.name + status;
       });
+
+
+    enter.append("text")
+      .attr("dy", ".31em")
+      .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
+      .attr("transform", function(d) { return d.x < 180 ? "translate(8)" : "rotate(180) translate(-8)"; })
+      .text(function(d) {
+        var status = d.status;
+        if (status < 1 && status > 0 && status != .5) {
+          status = Math.round(d.status * 100) + "%";
+        } else {
+          status = "";
+        }
+
+        if (d.name === "root") {
+          d3.select(".title").select("text").html(site + " (" + status + " complete)");
+        }
+        return d.name + " " + status;
+      })
 
     node.attr("transform", function(d) { return "rotate(" + (d.x - 90) + ") translate(" + d.y + ")"; })
 
@@ -97,7 +171,7 @@
       parent.length = parent.length - 1;
       node.parentNode.remove();
     }
-    redraw(root);
+    redraw("");
   }
 
   d3.select(self.frameElement).style("height", diameter + "px");
