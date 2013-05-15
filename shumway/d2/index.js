9344141e5ab498af15e3c21680ead9e297c38c13
Jeff Dyer <jeffdyer@acm.org>
2013-05-14 17:45:04 -0700

    update prose

--- shumway/d2/index.js
+++ shumway/d2/index.js
@@ -40,7 +40,7 @@ var redraw;
     "<b>This page shows the Shumway implementation status for the native Flash APIs ",
     "used by the top video sites.</b> Choose a video site to see the status of the ",
     "Shumway implementation. <font class='green'>Green is good</font>. ",
-    "<font class='red'>Red is bad</font>. <font class='orange'>Orange is ",
+    "<font class='red'>Red is bad</font>. <font class='yellow'>Yellow is ",
     "somewhere in between</font>. (% indicates completion rate, if not 100%, 50% or >10%.)",
     "The source code can be found here: ",
     "<a href='https://github.com/artcompiler/P105'>https://github.com/artcompiler/P105</a>. ",
