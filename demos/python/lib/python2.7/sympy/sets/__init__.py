5c3205b99dd9c35636db316bbac301d2ecd86725
Jeff Dyer <jeffdyer@acm.org>
2015-02-19 12:13:35 -0700

    Add sympy to python demo

---
+++ demos/python/lib/python2.7/sympy/sets/__init__.py
@@ -0,0 +1,4 @@
+from .sets import (Set, Interval, Union, EmptySet, FiniteSet, ProductSet,
+        Intersection, imageset, Complement, SymmetricDifference)
+from .fancysets import TransformationSet, ImageSet, Range
+from .contains import Contains
